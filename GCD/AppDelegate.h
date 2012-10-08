ab8150fb47d9816a6c24d09e2e50058263be939e
裴洪凯 <peihongkai@peimatoMacBook-Air.local>
2012-10-08 16:38:41 +0800

    Initial Commit

---
+++ GCD/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GCD
+//
+//  Created by 裴洪凯 on 12-10-8.
+//  Copyright (c) 2012年 裴洪凯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GCD/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GCD
+//
+//  Created by 裴洪凯 on 12-10-8.
+//  Copyright (c) 2012年 裴洪凯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
